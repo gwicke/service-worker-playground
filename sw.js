9507cf467f8a41b180d1369790ff0f892117ade2
Gabriel Wicke <gwicke@wikimedia.org>
2015-09-10 17:10:43 -0700

    Formatting cleanup

--- sw.js
+++ sw.js
@@ -6,29 +6,28 @@ var tplURL = '/wiki/Test';
 var tpl;
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        fetch(tplURL, { credentials: 'include' })
-        .then(res => replaceContent(res.text(), ''))
-        .then(body => cache.put(tplURL, new Response(body)));
-      })
-  );
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache =>
+            fetch(tplURL, { credentials: 'include' })
+            .then(res => replaceContent(res.text(), ''))
+            .then(body => cache.put(tplURL, new Response(body))))
+    );
 });
 
 function fetchBody(req, title) {
-  var protoHost = req.url.match(/^(https?:\/\/[^\/]+)\//)[1];
-  return fetch(protoHost + '/api/rest_v1/page/html/' + title)
-    .then(res => res.text());
+    var protoHost = req.url.match(/^(https?:\/\/[^\/]+)\//)[1];
+    return fetch(protoHost + '/api/rest_v1/page/html/' + title)
+        .then(res => res.text());
 }
 
 function getTemplate() {
-  return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.match(new Request(tplURL))
-        .then(resp => resp.text());
-    });
+    return caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.match(new Request(tplURL))
+                .then(resp => resp.text());
+        });
 }
 
 function cheapBodyInnerHTML(html) {
@@ -41,9 +40,10 @@ function cheapBodyInnerHTML(html) {
 }
 
 function replaceContent(tpl, content) {
-  return tpl.replace(/(<div id="mw-content-text"[^>]*>)[\s\S]*(<div class="printfooter")/im, function(all, start, end) {
-    return start + content + end;
-  });
+    var bodyMatcher = /(<div id="mw-content-text"[^>]*>)[\s\S]*(<div class="printfooter")/im;
+    return tpl.replace(bodyMatcher, function(all, start, end) {
+        return start + content + end;
+    });
 }
 
 var escapes = {
@@ -53,39 +53,37 @@ var escapes = {
 };
 
 function injectBody(tpl, body, req, title) {
-  // Hack hack hack..
-  // In a real implementation, this will
-  // - identify page components in a template,
-  // - evaluate and each component, and
-  // - stream expanded template parts / components as soon as they are
-  //   available.
-  tpl = tpl.replace(/Test/g, title.replace(/[<"']/g, s => escapes[s]));
-  // Styles
-  tpl = tpl.replace(/modules=/, 'modules=ext.cite.style%7C');
-  tpl = tpl.replace(/\/wiki\//g, '/w/iki/');
-  return replaceContent(tpl, cheapBodyInnerHTML(body));
+    // Hack hack hack..
+    // In a real implementation, this will
+    // - identify page components in a template,
+    // - evaluate and each component, and
+    // - stream expanded template parts / components as soon as they are
+    //   available.
+    tpl = tpl.replace(/Test/g, title.replace(/[<"']/g, s => escapes[s]));
+    // Styles
+    tpl = tpl.replace(/modules=/, 'modules=ext.cite.style%7C');
+    tpl = tpl.replace(/\/wiki\//g, '/w/iki/');
+    return replaceContent(tpl, cheapBodyInnerHTML(body));
 }
 
 function assemblePage(req) {
-  var title = req.url.match(/\/w\/iki\/([^?]+)$/)[1];
-  return Promise.all([getTemplate(), fetchBody(req, title)])
-    .then(results => injectBody(results[0], results[1], req, title));
+    var title = req.url.match(/\/w\/iki\/([^?]+)$/)[1];
+    return Promise.all([getTemplate(), fetchBody(req, title)])
+        .then(results => injectBody(results[0], results[1], req, title));
 }
 
 self.addEventListener('fetch', function(event) {
-  //console.log(event.request.url);
-  if (/\/w\/iki\/[^?]+$/.test(event.request.url)) {
-    //console.log('fetching', event.request.url);
-    return event.respondWith(
-        // Ideally, we'd start to stream the header right away here.
-        assemblePage(event.request)
-        .then(body => {
-          return new Response(body, {
-            headers: {
-              'content-type': 'text/html;charset=utf-8'
-            }
-          });
-        })
-    );
-  }
+    //console.log(event.request.url);
+    if (/\/w\/iki\/[^?]+$/.test(event.request.url)) {
+        //console.log('fetching', event.request.url);
+        return event.respondWith(
+            // Ideally, we'd start to stream the header right away here.
+            assemblePage(event.request)
+            .then(body => new Response(body, {
+                headers: {
+                    'content-type': 'text/html;charset=utf-8'
+                }
+            }))
+        );
+    }
 });
